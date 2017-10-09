8a0d7b77286916edcab568f058f924e08aa403fc
isis97 <piotrsty1@gmail.com>
2017-10-09 13:00:05 +0200

    Add wrong parameters count error handling

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+inline void print_usage_message() {
+	std::cout << "Usage: PROGRAM file\n";
+}
+
 int main(int argc, char** argv) {
-	std::cout << "Hello World\n";
+
+    if(argc != 1) {
+      //TODO: Throw error
+			print_usage_message();
+			return 1;
+		}
+
+    std::cout << "Hello World\n";
+
+    return 0;
 }
