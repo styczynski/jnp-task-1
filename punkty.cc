db7123a88ff856902953f1b8903db2083ca27dd8
joald <jacek.olczyk98@gmail.com>
2017-10-09 13:20:29 +0200

    added program structure

--- punkty.cc
+++ punkty.cc
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 
 inline void print_usage_message(const std::string& program_name) {
 	std::cout << "Usage: " << program_name << " file\n";
+}
+
+auto read_student_list() {
+    //returns std::vector<std::tuple<std::string, int>>
+    // TODO styczyn
+}
+
+auto read_groups(const std::vector<std::tuple<std::string, int>>& students) {
+    // TODO joald
+}
+
+void print_bad_students(void, void) {
+
 }
 
 int main(int argc, char** argv) {
@@ -11,6 +25,9 @@ int main(int argc, char** argv) {
 		print_usage_message(argv[0]);
 		return 1;
 	}
+	auto students = read_student_list();
+	auto groups = read_groups(students);
+    print_bad_students(students, groups);
 
     std::cout << "Hello World\n";
 
