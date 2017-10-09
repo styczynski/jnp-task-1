a458cdd4d9c8a318c7c5a211c41fecdb9eb171db
joald <jacek.olczyk98@gmail.com>
2017-10-09 13:07:29 +0200

    Fixed print_usage_message

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-inline void print_usage_message() {
-	std::cout << "Usage: PROGRAM file\n";
+inline void print_usage_message(const std::string& program_name) {
+	std::cout << "Usage: " << program_name << " file\n";
 }
 
 int main(int argc, char** argv) {
 
     if(argc != 1) {
-      //TODO: Throw error
-			print_usage_message();
-			return 1;
-		}
+		//TODO: Throw error
+		print_usage_message(argv[0]);
+		return 1;
+	}
 
     std::cout << "Hello World\n";
 
