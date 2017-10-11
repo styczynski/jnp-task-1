c365ec1492decbbe459dad841e99ac45d2633389
styczynski <piotrsty1@gmail.com>
2017-10-11 18:51:26 +0200

    CRLF->LF, fix indentation, fix tabs vs spaces

--- punkty.cc
+++ punkty.cc
@@ -22,57 +22,57 @@ inline void print_error_in_file(const std::string &filename, unsigned int line_n
 }
 
 inline static bool is_digit(const char& c) {
-	return (c>='0') && (c<='9');
+    return (c>='0') && (c<='9');
 }
 
 inline static bool is_low_case_letter(const char& c) {
-	return (c>='a') && (c<='z');
+    return (c>='a') && (c<='z');
 }
 
 // TODO: Handle overflow anyway there below
 // (I know, yes I know we ARE overkilling now...)
 inline static std::optional<int> parse_non_negative_integer(const std::string& input, const int starting_index=0) {
-	const int len = input.size();
-	int result = 0;
-	for(int i=starting_index; i<len; ++i) {
-		if(!is_digit(input[i])) {
-			// Error: non digit character in input string!
-			return std::optional<int>();
-		} else {
-			result = result * 10 + (input[i]-'0');
-		}
-	}
-	return std::optional<int>(result);
+    const int len = input.size();
+    int result = 0;
+    for(int i=starting_index; i<len; ++i) {
+        if(!is_digit(input[i])) {
+            // Error: non digit character in input string!
+            return std::optional<int>();
+        } else {
+            result = result * 10 + (input[i]-'0');
+        }
+    }
+    return std::optional<int>(result);
 }
-	
+  
 std::optional<int> get_student_id(const std::string& id) {
-	// Invalid length
-	if(id.size() < 8) return std::optional<int>();
-	
-	// Two first are letters (lowercase, english alphabet)
-	if(!is_low_case_letter(id[0]) ||
-     !is_low_case_letter(id[1])) {
-		return std::optional<int>();
-	}
-	
-	// We test third character
-	if(is_digit(id[2])) {
-		// If we have digit we try to match FULL ID form
-		// xx000000
-		
-		// Everything left should be a valid number
-		return parse_non_negative_integer(id, 3);
-	} else {
-		// If not we try to match DASHED ID form
-		// xxm-0000
-		
-		if(id[3] != 'm') return std::optional<int>();
-		if(id[4] != '-') return std::optional<int>();
-		
-		// Everything left should be a valid number
-		return parse_non_negative_integer(id, 5);
-	}
-	
+    // Invalid length
+    if(id.size() < 8) return std::optional<int>();
+    
+    // Two first are letters (lowercase, english alphabet)
+    if(!is_low_case_letter(id[0]) ||
+       !is_low_case_letter(id[1])) {
+        return std::optional<int>();
+    }
+    
+    // We test third character
+    if(is_digit(id[2])) {
+        // If we have digit we try to match FULL ID form
+        // xx000000
+        
+        // Everything left should be a valid number
+        return parse_non_negative_integer(id, 3);
+    } else {
+        // If not we try to match DASHED ID form
+        // xxm-0000
+        
+        if(id[3] != 'm') return std::optional<int>();
+        if(id[4] != '-') return std::optional<int>();
+        
+        // Everything left should be a valid number
+        return parse_non_negative_integer(id, 5);
+    }
+  
 }
 
 bool verify_id(const std::string &id) {
@@ -84,26 +84,26 @@ std::vector<Student> read_student_list(const char* filename) {
     std::ifstream input_stream(filename);
     
     if(input_stream.is_open()) {
-      std::string input_line;
-			unsigned int line_number = 1;
-      while(std::getline(input_stream, input_line)) {
-				const std::optional<int> student_id = get_student_id(input_line);
-				if(student_id.has_value()) {
-					student_list.push_back(std::make_tuple(input_line, student_id.value()));
-				} else {
-					print_error_in_file(std::string(filename), line_number, input_line);
-				}
-				++line_number;
-      }
-			
-      input_stream.close();
+        std::string input_line;
+        unsigned int line_number = 1;
+        while(std::getline(input_stream, input_line)) {
+            const std::optional<int> student_id = get_student_id(input_line);
+            if(student_id.has_value()) {
+                student_list.push_back(std::make_tuple(input_line, student_id.value()));
+            } else {
+                print_error_in_file(std::string(filename), line_number, input_line);
+            }
+            ++line_number;
+        }
+          
+        input_stream.close();
     } else {
-			// TODO: File couldn't be loaded -> throw error
-			std::cout<<"Could not read file!\n";
-		}
-		
-		
-		return student_list;
+        // TODO: File couldn't be loaded -> throw error
+        std::cout<<"Could not read file!\n";
+    }
+  
+  
+    return student_list;
 }
 
 bool check_basic_group_validity(const std::string &line) {
@@ -118,7 +118,7 @@ bool check_basic_group_validity(const std::string &line) {
 }
 
 Student parse_student(const std::string &id) {
-		return make_tuple(id, 0);
+    return make_tuple(id, 0);
     //return {id, 0}; //TODO placeholder
 }
 
@@ -165,7 +165,7 @@ std::vector<Group> read_groups(const std::vector<Student> &students) {
 }
 
 void print_bad_students(const std::vector<Student> &students,
-												const std::vector<Group> &groups) {
+                        const std::vector<Group> &groups) {
     std::vector<int> deducted_points(students.size());
 
     // TODO implement the rest
@@ -184,10 +184,10 @@ int main(int argc, char **argv) {
         print_usage_message(argv[0]);
         return 1;
     }
-		
-		auto students = read_student_list(argv[1]);
-		auto groups = read_groups(students);
-		print_bad_students(students, groups);
-		
+    
+    auto students = read_student_list(argv[1]);
+    auto groups = read_groups(students);
+    print_bad_students(students, groups);
+    
     return 0;
 }
